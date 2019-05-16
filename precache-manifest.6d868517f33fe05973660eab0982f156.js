3b69ffa9c04c7602333db55b277126dc336bd32c
Jacob Perron <jacobmperron@gmail.com>
2019-05-15 22:49:07 -0700

    Updates

---
+++ precache-manifest.6d868517f33fe05973660eab0982f156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16a3352d621d95b6ec8eca9879f2523",
+    "url": "/blintz/index.html"
+  },
+  {
+    "revision": "0089a8a356986bce9f3f",
+    "url": "/blintz/static/css/main.e7c05008.chunk.css"
+  },
+  {
+    "revision": "f0db02a20016bc7410a8",
+    "url": "/blintz/static/js/2.ed56b676.chunk.js"
+  },
+  {
+    "revision": "0089a8a356986bce9f3f",
+    "url": "/blintz/static/js/main.93c5a9bf.chunk.js"
+  },
+  {
+    "revision": "bc4afc68489a3dd0e43a",
+    "url": "/blintz/static/js/runtime~main.63b14ee5.js"
+  }
+]);
